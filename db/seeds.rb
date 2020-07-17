452b8c31513dbd09664c5ba65e3b4fec2eaa052b
Chandler Hulstrom <chulstro11@gmail.com>
2020-07-17 11:20:48 -0600

    Added seed and flushed out model testing

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+park_1 = AmusementPark.create(name: "Wally World", price: 30)
+mechanic_1 = Mechanic.create(name: "Johnny Apple", experience: 13)
+ride_1 = park_1.rides.create(name: "Wing Flyer", rating: 7)
+ride_2 = park_1.rides.create(name: "Lovely Floof", rating: 3)
+ride_3 = park_1.rides.create(name: "The Nightmare", rating: 10)
+
+RideMechanic.create(ride_id: ride_1.id, mechanic_id: mechanic_1.id)
+RideMechanic.create(ride_id: ride_2.id, mechanic_id: mechanic_1.id)
--- spec/models/amusement_park_spec.rb
+++ spec/models/amusement_park_spec.rb
@@ -4,4 +4,18 @@ RSpec.describe AmusementPark, type: :model do
   describe "relationships" do
     it {should have_many :rides}
   end
+
+  describe "methods" do
+    it ".thrill_avg" do
+      park_1 = AmusementPark.create(name: "Wally World", price: 30)
+      mechanic_1 = Mechanic.create(name: "Johnny Apple", experience: 13)
+      ride_1 = park_1.rides.create(name: "Wing Flyer", rating: 7)
+      ride_2 = park_1.rides.create(name: "Lovely Floof", rating: 3)
+
+      RideMechanic.create(ride_id: ride_1.id, mechanic_id: mechanic_1.id)
+      RideMechanic.create(ride_id: ride_2.id, mechanic_id: mechanic_1.id)
+
+      expect(park_1.thrill_avg).to eq(5)
+    end
+  end
 end
